582dc8518fcb1cf296b24b03080b7d210635b98e
IsmaelSWO <ismael.ulalam@softwareone.com>
2021-10-17 21:50:44 +0200

    cors

--- app.js
+++ app.js
@@ -20,9 +20,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  /* res.setHeader('Access-Control-Allow-Origin', '*'); */
-  res.setHeader('mode','no-cors');
+/* app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
@@ -30,7 +29,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
   next();
-});
+}); */
 
 /* app.use(cors()); */
 app.use("/api/players", playersRoutes);
