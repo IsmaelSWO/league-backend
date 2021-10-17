241e4a9f3aa23ad1dbdff0a5225b2c89abeaa240
IsmaelSWO <ismael.ulalam@softwareone.com>
2021-10-18 00:21:29 +0200

    cors

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const router = express.Router(); 
-/* const cors = require("cors");
-router.use(cors()); */ 
+const cors = require("cors");
+router.use(cors()); 
 
 const playersRoutes = require("./routes/players-routes");
 const ofertasRoutes = require("./routes/ofertas-routes");
@@ -21,7 +21,7 @@ const app = express();
 app.use(bodyParser.json());
 /* app.use(express.json()); */
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
 
   next();
-});
+}); */
 
 /* app.use(cors()); */
 app.use("/api/players", playersRoutes);
