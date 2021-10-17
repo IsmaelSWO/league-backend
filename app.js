b995bf77d16ddcda92867a0c4c6531488c71938b
IsmaelSWO <ismael.ulalam@softwareone.com>
2021-10-17 19:36:27 +0200

    new card desig

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
+    'mode : no-cors',
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
